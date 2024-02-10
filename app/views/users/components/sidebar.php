dcb5063bcc785906a2bdc8a96c16dc40e5c3b43f
badredddineazzaz <84666903+badredddineazzaz@users.noreply.github.com>
2024-02-10 14:33:36 +0100

    Ui modification | define constants

--- app/views/users/components/sidebar.php
+++ app/views/users/components/sidebar.php
@@ -9,8 +9,14 @@ $userDetails = $data['userDetails'];
 
 <!-- h-screen fixed top-0 left-0 min-w-[250px]  font-[sans-serif] overflow-auto fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 -->
 
+<<<<<<< Updated upstream
 <div class="flex flex-wrap items-center cursor-pointer">
         <img src='<?php echo IMG_LOAD . '/profile/' . $userDetails["image"]?>' class="w-10 h-10 rounded-full border-2 border-white" />
+=======
+
+<div class="flex flex-wrap items-center cursor-pointer">
+        <img src='<?php echo IMG_LOAD . '/profile/' . $userDetails["image"]?>' class="w-10 h-10 rounded-full object-cover	  border-2 border-white" />
+>>>>>>> Stashed changes
         <div class="ml-4">
           <p class="text-sm text-white"><?php echo (strtoupper($userDetails['lastname']) ." ". ucfirst($userDetails['firstname'])) ?></p>
           <p class="text-xs text-gray-300 mt-1"><?php echo $userDetails['email'] ?></p>
@@ -18,6 +24,7 @@ $userDetails = $data['userDetails'];
       </div>
       <ul class="space-y-3 mt-10">
         <li>
+<<<<<<< Updated upstream
           <a href="<?php echo BASE_URL."dashboard" ?>" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-3 transition-all">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-[18px] h-[18px] mr-4"
               viewBox="0 0 512 512">
@@ -25,10 +32,15 @@ $userDetails = $data['userDetails'];
                 d="M197.332 170.668h-160C16.746 170.668 0 153.922 0 133.332v-96C0 16.746 16.746 0 37.332 0h160c20.59 0 37.336 16.746 37.336 37.332v96c0 20.59-16.746 37.336-37.336 37.336zM37.332 32A5.336 5.336 0 0 0 32 37.332v96a5.337 5.337 0 0 0 5.332 5.336h160a5.338 5.338 0 0 0 5.336-5.336v-96A5.337 5.337 0 0 0 197.332 32zm160 480h-160C16.746 512 0 495.254 0 474.668v-224c0-20.59 16.746-37.336 37.332-37.336h160c20.59 0 37.336 16.746 37.336 37.336v224c0 20.586-16.746 37.332-37.336 37.332zm-160-266.668A5.337 5.337 0 0 0 32 250.668v224A5.336 5.336 0 0 0 37.332 480h160a5.337 5.337 0 0 0 5.336-5.332v-224a5.338 5.338 0 0 0-5.336-5.336zM474.668 512h-160c-20.59 0-37.336-16.746-37.336-37.332v-96c0-20.59 16.746-37.336 37.336-37.336h160c20.586 0 37.332 16.746 37.332 37.336v96C512 495.254 495.254 512 474.668 512zm-160-138.668a5.338 5.338 0 0 0-5.336 5.336v96a5.337 5.337 0 0 0 5.336 5.332h160a5.336 5.336 0 0 0 5.332-5.332v-96a5.337 5.337 0 0 0-5.332-5.336zm160-74.664h-160c-20.59 0-37.336-16.746-37.336-37.336v-224C277.332 16.746 294.078 0 314.668 0h160C495.254 0 512 16.746 512 37.332v224c0 20.59-16.746 37.336-37.332 37.336zM314.668 32a5.337 5.337 0 0 0-5.336 5.332v224a5.338 5.338 0 0 0 5.336 5.336h160a5.337 5.337 0 0 0 5.332-5.336v-224A5.336 5.336 0 0 0 474.668 32zm0 0"
                 data-original="#000000" />
             </svg>
+=======
+          <a href="<?php echo BASE_URL."dashboard" ?>" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-2 transition-all">
+            <i class="fa-light fa-objects-column  mr-4 text-base"></i>
+>>>>>>> Stashed changes
             <span>Dashboard</span>
           </a>
         </li>
         <li>
+<<<<<<< Updated upstream
           <a href="javascript:void(0)" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-3 transition-all">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-[18px] h-[18px] mr-4" viewBox="0 0 16 16">
               <path
@@ -109,13 +121,50 @@ $userDetails = $data['userDetails'];
                 d="M437.02 74.98C388.668 26.63 324.379 0 256 0S123.332 26.629 74.98 74.98C26.63 123.332 0 187.621 0 256s26.629 132.668 74.98 181.02C123.332 485.37 187.621 512 256 512s132.668-26.629 181.02-74.98C485.37 388.668 512 324.379 512 256s-26.629-132.668-74.98-181.02zM111.105 429.297c8.454-72.735 70.989-128.89 144.895-128.89 38.96 0 75.598 15.179 103.156 42.734 23.281 23.285 37.965 53.687 41.742 86.152C361.641 462.172 311.094 482 256 482s-105.637-19.824-144.895-52.703zM256 269.507c-42.871 0-77.754-34.882-77.754-77.753C178.246 148.879 213.13 114 256 114s77.754 34.879 77.754 77.754c0 42.871-34.883 77.754-77.754 77.754zm170.719 134.427a175.9 175.9 0 0 0-46.352-82.004c-18.437-18.438-40.25-32.27-64.039-40.938 28.598-19.394 47.426-52.16 47.426-89.238C363.754 132.34 315.414 84 256 84s-107.754 48.34-107.754 107.754c0 37.098 18.844 69.875 47.465 89.266-21.887 7.976-42.14 20.308-59.566 36.542-25.235 23.5-42.758 53.465-50.883 86.348C50.852 364.242 30 312.512 30 256 30 131.383 131.383 30 256 30s226 101.383 226 226c0 56.523-20.86 108.266-55.281 147.934zm0 0"
                 data-original="#000000" />
             </svg>
+=======
+          <a href="<?php echo BASE_URL."dashboard" ?>" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-2 transition-all">
+            <i class="fa-light fa-pot-food  mr-4 text-base"></i>
+            <span>Recettes</span>
+          </a>
+        </li>
+        <li>
+          <a href="<?php echo BASE_URL."dashboard" ?>" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-2 transition-all">
+            <i class="fa-light fa-user-chef  mr-4 text-base"></i>
+            <span>Nutritionnistes</span>
+          </a>
+        </li>
+        
+        
+        <li>
+          <a href="javascript:void(0)" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-2 transition-all">
+            <i class="fa-light fa-star mr-4 text-base"></i>
+            <span>Favoris</span>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0)" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-2 transition-all">
+            <i class="fa-light fa-calendar mr-4 text-base"></i>
+            <span>Plan</span>
+          </a>
+        </li>
+        
+        
+        <li>
+          <a href="<?php echo BASE_URL."dashboard/updateprofile" ?>" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-3 transition-all">
+            <i class="fa-light fa-user mr-4 text-base"></i>
+
+>>>>>>> Stashed changes
             <span>Profile</span>
           </a>
         </li>
       </ul>
 
       <a href="<?php echo BASE_URL."dashboard/logout" ?>" class="text-white text-sm flex items-center hover:bg-gray-700 rounded px-4 py-3 transition-all my-20">
+<<<<<<< Updated upstream
       <svg xmlns="http://www.w3.org/2000/svg" class="w-[18px] h-[18px] mr-4" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+=======
+        <i class="fa-light fa-right-from-bracket mr-3 text-lg"></i>
+>>>>>>> Stashed changes
             <span>Se déconnecter</span>
       </a>
     </nav>
--- app/views/users/editprofile.php
+++ app/views/users/editprofile.php
@@ -12,7 +12,7 @@
     
     <div class="flex flex-wrap items-center justify-between mb-10 space-y-3">
         <div class="left flex items-center">
-            <img class="rounded-full h-32 w-32 mr-5" src="<?php echo IMG_LOAD . '/profile/' . $userDetails["image"]?>" alt="">
+            <img class="rounded-full h-32 w-32 mr-5 p-1 object-cover	 ring-2 ring-gray-300 dark:ring-gray-500" src="<?php echo IMG_LOAD . '/profile/' . $userDetails["image"]?>" alt="">
             <div class="flex flex-col">
                 <div class="name text-xl"><?php echo (strtoupper($userDetails['lastname']) ." ". ucfirst($userDetails['firstname'])) ?></div>
                 <div class="username text-sm italic text-slate-500	">@<?php echo $userDetails['username'] ?></div>
@@ -25,51 +25,104 @@
     </div>
     <hr class="mb-10">
 
-              <form class="space-y-4 md:space-y-6" action="signup" method="post">
-
-            <?php if (isset($error) && !empty($error)) : ?>
-                <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-sm p-4 mb-2 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                    <?php echo $error;  ?>
-                </div>
-            <?php endif;?>
-                <div class="flex space-x-4">
-                    <div class="flex-1">
-                        <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
-                        <input  type="text" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" value="<?php echo $userDetails['firstname']?>" required="">
-                    </div>
-                    <div class="flex-1">
-                        <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
-                        <input type="text" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" value="<?php echo $userDetails['lastname']?>" required="">
-                    </div>
-                </div>
-                <div>
-                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom d'utilisateur</label>
-                      <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john_doe" value="<?php echo $userDetails['username']?>" required="">
-                </div>
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" value="<?php echo $userDetails['email']?>" required="">
-                  </div>
-
-                  <label for="sexe" class="block  text-sm font-medium text-gray-900 dark:text-white">Sexe</label>
-
-                  <ul class="flex space-x-4">
-                        <li class="flex-1">
-                            <input class="sr-only peer" <?php echo $userDetails['sexe']==="homme"? "checked":"" ?> type="radio" value="homme" name="sexe" id="homme">
-                            <label class="text-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-black-500 peer-checked:ring-2 peer-checked:border-transparent" for="homme">Homme</label>
 
 
-                        <li class="flex-1">
-                            <input class="sr-only peer" <?php echo $userDetails['sexe']==="femme"? "checked":"" ?> type="radio" value="femme" name="sexe" id="femme">
-                            <label class="text-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-black-500 peer-checked:ring-2 peer-checked:border-transparent" for="femme">Femme</label>
 
 
-                        </li>
-                    </ul>
+    
+<div id="accordion-arrow-icon" data-accordion="open">
+  <h2 id="accordion-arrow-icon-heading-1">
+    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-900 bg-gray-100 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-arrow-icon-body-1" aria-expanded="true" aria-controls="accordion-arrow-icon-body-1">
+      <span>Mes informations personnelles</span>
+      <i class="fa-solid fa-user"></i>
+    </button>
+  </h2>
+  <div id="accordion-arrow-icon-body-1" aria-labelledby="accordion-arrow-icon-heading-1">
+    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+    <form class="space-y-4 md:space-y-6" action="signup" method="post">
 
-                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Modifier</button>
+<?php if (isset($error) && !empty($error)) : ?>
+    <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-sm p-4 mb-2 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        <?php echo $error;  ?>
+    </div>
+<?php endif;?>
+    <div class="flex space-x-4">
+        <div class="flex-1">
+            <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
+            <input  type="text" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" value="<?php echo $userDetails['firstname']?>" required="">
+        </div>
+        <div class="flex-1">
+            <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+            <input type="text" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" value="<?php echo $userDetails['lastname']?>" required="">
+        </div>
+    </div>
+    <div>
+          <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom d'utilisateur</label>
+          <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john_doe" value="<?php echo $userDetails['username']?>" required="">
+    </div>
+      <div>
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
+          <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" value="<?php echo $userDetails['email']?>" required="">
+      </div>
 
-              </form>
+      <label for="sexe" class="block  text-sm font-medium text-gray-900 dark:text-white">Sexe</label>
+
+      <ul class="flex space-x-4">
+            <li class="flex-1">
+                <input class="sr-only peer" <?php echo $userDetails['sexe']==="homme"? "checked":"" ?> type="radio" value="homme" name="sexe" id="homme">
+                <label class="text-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-black-500 peer-checked:ring-2 peer-checked:border-transparent" for="homme">Homme</label>
+
+
+            <li class="flex-1">
+                <input class="sr-only peer" <?php echo $userDetails['sexe']==="femme"? "checked":"" ?> type="radio" value="femme" name="sexe" id="femme">
+                <label class="text-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-black-500 peer-checked:ring-2 peer-checked:border-transparent" for="femme">Femme</label>
+
+
+            </li>
+        </ul>
+
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Modifier</button>
+
+  </form>
+    </div>
+  </div>
+  <h2 id="accordion-arrow-icon-heading-2">
+    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-arrow-icon-body-2" aria-expanded="false" aria-controls="accordion-arrow-icon-body-2">
+      <span>Accordion with another icon</span>
+      <svg class="w-4 h-4 shrink-0 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+      </svg>
+    </button>
+  </h2>
+  <div id="accordion-arrow-icon-body-2" class="hidden" aria-labelledby="accordion-arrow-icon-heading-2">
+    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+      <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
+      <p class="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" class="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+    </div>
+  </div>
+  <h2 id="accordion-arrow-icon-heading-3">
+    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-arrow-icon-body-3" aria-expanded="false" aria-controls="accordion-arrow-icon-body-3">
+      <span>Accordion without arrow rotation</span>
+      <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+      </svg>
+    </button>
+  </h2>
+  <div id="accordion-arrow-icon-body-3" class="hidden" aria-labelledby="accordion-arrow-icon-heading-3">
+    <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+      <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+      <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+      <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+      <ul class="ps-5 text-gray-500 list-disc dark:text-gray-400">
+        <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+        <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+
+              
           </div>
       </div>
   </div>
